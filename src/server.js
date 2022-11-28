9ca61b6af191b6193e160e88795360b7ae20e6bf
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-11-28 10:54:17 +0900

    Message Event

--- src/server.js
+++ src/server.js
@@ -13,10 +13,13 @@ const handleListen = () => console.log(`Listen on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
-function handleConnection(socket){
-    console.log(socket);
-}
-
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from Browser"));
+    socket.on("message", (message) => {
+        console.log(message.toString("utf8"));
+    })
+    socket.send("Hello");
+});
 
 server.listen(3000, handleListen);
