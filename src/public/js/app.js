4fe9013925324b0d504e89aa3bfc27a44d6910d8
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-12-04 18:08:20 +0900

    3.7
    IceCandidate

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -114,19 +114,36 @@ socket.on("welcome", async () => {
 });
 // Browser2 code
 socket.on("offer", async (offer) => {
+  console.log("received the offer");
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
   myPeerConnection.setLocalDescription(answer);
   socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
 });
 // Browser1 code afer Browser2 code
 socket.on("answer", (answer) => {
+  console.log("receive the answer");
   myPeerConnection.setRemoteDescription(answer);
+});
+socket.on("ice", (ice) => {
+  console.log("received candidate");
+  myPeerConnection.addIceCandidate(ice);
 });
 // RTC Code
 function makeConnection() {
   myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
   myStream
     .getTracks()
     .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+function handleIce(data) {
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, room);
+}
+function handleAddStream(data) {
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.stream;
 }
