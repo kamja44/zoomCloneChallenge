79ac519fbb17d037b4c5be96ded1c82fd09268c0
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-12-04 20:23:23 +0900

    3.11
    Data Channels

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -9,6 +9,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 const call = document.getElementById("call");
 call.hidden = true;
 
@@ -114,6 +115,11 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 // Socket Code
 // Browser1 code
 socket.on("welcome", async () => {
+  myDataChannel = myPeerConnection.createDataChannel("chat");
+  myDataChannel.addEventListener("message", (event) => {
+    console.log(event.data);
+  });
+  console.log("make data channel");
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
   console.log("sent the offer");
@@ -121,6 +127,12 @@ socket.on("welcome", async () => {
 });
 // Browser2 code
 socket.on("offer", async (offer) => {
+  myPeerConnection.addEventListener("datachannel", (event) => {
+    myDataChannel = event.channel;
+    myDataChannel.addEventListener("message", (event) => {
+      console.log(event.data);
+    });
+  });
   console.log("received the offer");
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
@@ -139,7 +151,19 @@ socket.on("ice", (ice) => {
 });
 // RTC Code
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun: stun.l.google.com:19302",
+          "stun: stun1.l.google.com:19302",
+          "stun: stun2.l.google.com:19302",
+          "stun: stun3.l.google.com:19302",
+          "stun: stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener("icecandidate", handleIce);
   myPeerConnection.addEventListener("addstream", handleAddStream);
   myStream
